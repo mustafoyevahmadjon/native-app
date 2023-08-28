1dd6d3fe39a86d19a41db14d1d1454a5a621ee89
codercreator2005 <codercreator2005@gmail.com>
2023-08-28 17:26:55 +0500

    Location access

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet } from 'react-native';
 import Loader from './components/loader';
+import { useEffect, useState } from 'react';
+import Weather from './components/weather';
+import * as Location from 'expo-location';
 
 export default function App() {
-  return (
-    <Loader />
-  );
+  const [isloading, setisloading] = useState(true)
+  const [location, setlocation] = useState({
+    latitude: null,
+    longitude: null,
+  })
+
+  const getLocation = async () => {
+    try {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert('Permission to access location was denied');
+        return;
+      }
+      const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync({})
+      setlocation({
+        latitude: latitude,
+        longitude: longitude
+      })
+    } catch (error) {
+      Alert.alert("I can't find your current location, so bad ):")
+    }
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      setisloading(false)
+      getLocation()
+    }, 2000)
+  }, [])
+
+  return isloading ? <Loader /> : <Weather />
 }
 
 const styles = StyleSheet.create({});
